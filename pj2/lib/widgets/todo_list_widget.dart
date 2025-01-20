4868abf9af0b7ec8bfe2c3bbc2e04a1e3d973fc9
jhonnylucius <149340275+jhonnylucius@users.noreply.github.com>
2025-01-19 22:39:09 -0300

    refatora o builder do widget de lista de tarefas para usar 'todoList' em vez de 'value'

--- pj2/lib/widgets/todo_list_widget.dart
+++ pj2/lib/widgets/todo_list_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:pj2/controller/todo_list_controller.dart';
-import 'package:pj2/model/todo.dart';
 import 'package:pj2/widgets/todo_item_widget.dart';
 
 class TodoListWidget extends StatelessWidget {
@@ -12,7 +11,7 @@ class TodoListWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return ValueListenableBuilder(
       valueListenable: controller.todoListNotifier,
-      builder: (context, value, child) {
+      builder: (context, todoList, child) {
         return ListView.builder(
           primary: false,
           shrinkWrap: true,
