38d61abeef43a7831de7dd46514d3160229cdeb0
jhonnylucius <149340275+jhonnylucius@users.noreply.github.com>
2025-01-20 13:28:25 -0300

    adiciona métodos de atualização, alternância e remoção ao controlador de lista de tarefas

--- pj2/lib/controller/todo_list_controller.dart
+++ pj2/lib/controller/todo_list_controller.dart
@@ -2,4 +2,23 @@ import 'package:pj2/notifiers/todo_list_notifier.dart';
 
 class TodoListController {
   final todoListNotifier = TodoListNotifier();
+
+  void update(
+    String id,
+    String task,
+    bool completed,
+  ) {
+    todoListNotifier.update(
+      id,
+      task,
+    );
+  }
+
+  void toggle(String id) {
+    todoListNotifier.toggle(id);
+  }
+
+  void remove(String id) {
+    todoListNotifier.remove(id);
+  }
 }
