f93bf1c6292f07e4953efcb4c5365a80034ecfe4
jhonnylucius <149340275+jhonnylucius@users.noreply.github.com>
2025-01-20 17:00:50 -0300

    adiciona suporte a filtros na tela de lista de tarefas e atualiza a interface do usuário

--- pj2/lib/screens/todo_list_screen.dart
+++ pj2/lib/screens/todo_list_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:pj2/controller/todo_list_controller.dart';
+import 'package:pj2/model/todo_filter.dart';
+import 'package:pj2/service/service_locator.dart';
 import 'package:pj2/widgets/new_todo_widget.dart';
 import 'package:pj2/widgets/todo_list_widget.dart';
 
@@ -10,17 +13,48 @@ class TodoListScreen extends StatefulWidget {
 }
 
 class _TodoListScreen extends State<TodoListScreen> {
+  final controller = getIt<TodoListController>();
+  static const List<Tab> tabs = [
+    Tab(text: 'Todas'),
+    Tab(text: 'A Fazer'),
+    Tab(text: 'Concluida'),
+  ];
+
+  @override
+  void initState() {
+    controller.init();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text('To Do App'),
+    return DefaultTabController(
+      length: tabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            title: Text('To Do App'),
+            bottom: TabBar(
+              tabs: tabs,
+              onTap: (index) {
+                controller.changeFilter(TodoFilter.values[index]);
+              },
+            )),
+        body: ListView(
+          children: [
+            ValueListenableBuilder(
+              valueListenable: controller.filterNotifier,
+              builder: (context, filter, child) {
+                if (filter != TodoFilter.completed) {
+                  return const SizedBox.shrink();
+                }
+                return const NewTodoWidget();
+              },
+            ),
+            TodoListWidget(),
+          ],
+        ),
       ),
-      body: ListView(children: [
-        const NewTodoWidget(),
-        TodoListWidget(),
-      ]),
     );
   }
 }
