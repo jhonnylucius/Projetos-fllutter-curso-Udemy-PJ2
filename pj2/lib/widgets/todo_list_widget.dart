adcea453dc96a2c1b2effcdc20073ec656831ef2
jhonnylucius <149340275+jhonnylucius@users.noreply.github.com>
2025-01-19 23:23:20 -0300

    add file

--- pj2/lib/widgets/todo_list_widget.dart
+++ pj2/lib/widgets/todo_list_widget.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:get_it/get_it.dart';
 import 'package:pj2/controller/todo_list_controller.dart';
+import 'package:pj2/service/service_locator.dart';
 import 'package:pj2/widgets/todo_item_widget.dart';
 
 class TodoListWidget extends StatelessWidget {
   TodoListWidget({super.key});
 
-  final controller = GetIt<TodoListController>();
+  final controller = getIt<TodoListController>();
 
   @override
   Widget build(BuildContext context) {
